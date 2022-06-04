c2cbe197fe9ac45761fae2bc96295d80ba12c46c
Juan Diaz <juanchodis@hotmail.com>
2022-06-03 20:05:28 -0400

    enter key event in JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,46 @@
-const videoCardContainer = document.querySelector('.video-container');
+const videoCardContainer = document.querySelector(".video-container");
 
 let api_key = "your api key";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
-fetch(video_http + new URLSearchParams({
-    key: api_key,
-    part: 'snippet',
-    chart: 'mostPopular',
-    maxResults: 50,
-    regionCode: 'IN'
-}))
-.then(res => res.json())
-.then(data => {
-    data.items.forEach(item => {
-        getChannelIcon(item);
+fetch(
+  video_http +
+    new URLSearchParams({
+      key: api_key,
+      part: "snippet",
+      chart: "mostPopular",
+      maxResults: 50,
+      regionCode: "IN",
     })
-})
-.catch(err => console.log(err));
+)
+  .then((res) => res.json())
+  .then((data) => {
+    data.items.forEach((item) => {
+      getChannelIcon(item);
+    });
+  })
+  .catch((err) => console.log(err));
 
 const getChannelIcon = (video_data) => {
-    fetch(channel_http + new URLSearchParams({
+  fetch(
+    channel_http +
+      new URLSearchParams({
         key: api_key,
-        part: 'snippet',
-        id: video_data.snippet.channelId
-    }))
-    .then(res => res.json())
-    .then(data => {
-        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-        makeVideoCard(video_data);
-    })
-}
+        part: "snippet",
+        id: video_data.snippet.channelId,
+      })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      video_data.channelThumbnail =
+        data.items[0].snippet.thumbnails.default.url;
+      makeVideoCard(video_data);
+    });
+};
 
 const makeVideoCard = (data) => {
-    videoCardContainer.innerHTML += `
+  videoCardContainer.innerHTML += `
     <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
         <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
         <div class="content">
@@ -45,16 +52,23 @@ const makeVideoCard = (data) => {
         </div>
     </div>
     `;
-}
+};
 
 // search bar
 
-const searchInput = document.querySelector('.search-bar');
-const searchBtn = document.querySelector('.search-btn');
+const searchInput = document.querySelector(".search-bar");
+const searchBtn = document.querySelector(".search-btn");
 let searchLink = "https://www.youtube.com/results?search_query=";
 
-searchBtn.addEventListener('click', () => {
-    if(searchInput.value.length){
-        location.href = searchLink + searchInput.value;
-    }
-})
+searchBtn.addEventListener("click", () => {
+  if (searchInput.value.length) {
+    location.href = searchLink + searchInput.value;
+  }
+});
+
+// ENTER Key:
+searchInput.addEventListener("keyup", (event) => {
+  if (event.which === 13) {
+    location.href = searchLink + searchInput.value;
+  }
+});
